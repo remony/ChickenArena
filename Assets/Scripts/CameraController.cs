40584fcfde005ccb03e71237850f42a20acb1fa3
remony <stuartjdouglas@icloud.com>
2019-05-04 17:11:50 +0100

    Base commit: Includes base plugins (github) InputManager change for controller Basic terrain Player controller Camera controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+
+    public float distance = 6f;
+
+    public float verticalTurnSpeed = 4.0f;
+    public float horizontalTurnSpeed = 2.0f;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    /** Called after each frame update */
+    void LateUpdate()
+    {
+        offset = Quaternion.AngleAxis(Input.GetAxis("Vertical2") * verticalTurnSpeed, Vector3.up) * offset;
+        offset = Quaternion.AngleAxis(Input.GetAxis("Horizontal2") * horizontalTurnSpeed, Vector3.right) * offset;
+        transform.position = player.transform.position + offset;
+        transform.LookAt(player.transform.position);
+    }
+
+}
---
+++ Assets/Scripts/ThirdPersonController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonController : MonoBehaviour
+{
+    public Transform camera;
+    public float speed = 6f;
+
+    public float jumpSpeed = 8f;
+
+    public float gravity = 20f;
+
+    public float rotationSpeed = 6f;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+    private CharacterController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {       
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+    
+    float direction = 0;
+    direction = Mathf.Atan2(horizontal, vertical) * Mathf.Rad2Deg;
+        direction += camera.eulerAngles.y;
+
+
+
+        if (controller.isGrounded) {
+
+                
+
+            // moveDirection = transform.forward * speed * Time.deltaTime;
+            Vector3 horizontalM = Vector3.zero;
+            Vector3 verticalM = Vector3.zero;;
+            if (horizontal > 0) {
+                horizontalM = transform.forward * horizontal;
+            } else if (horizontal < 0) {
+                horizontalM = (transform.forward * -1) * horizontal;
+            }
+
+            if (vertical > 0) {
+                verticalM = transform.forward * vertical;
+            } else if (vertical < 0) {
+                verticalM = (transform.forward * -1) * vertical;
+            }
+            moveDirection = horizontalM + verticalM;
+            // moveDirection = new Vector3(horizontal, 0, vertical);
+            // transform.TransformDirection(Vector3.forward);
+     
+            moveDirection *= speed;
+
+            if (Input.GetButton("Jump")) {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+ 
+    if (horizontal != 0 || vertical != 0) {
+        
+        transform.rotation = Quaternion.Slerp(
+        transform.rotation, 
+        Quaternion.Euler (0, direction, 0),
+        rotationSpeed * Time.deltaTime);
+    }
+
+        // Vector3 nextDir = new Vector3(horizontal, 0, vertical);
+// transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, 0, transform.localEulerAngles.z);
+    // float angle = Mathf.Atan2(vertical, horizontal) * Mathf.Rad2Deg;
+
+// transform.rotation = Quaternion.Euler(new Vector3(0,angle,  0));
+        // if (nextDir != Vector3.zero) {
+            // transform.rotation = camera.TransformDirection(Quaternion.LookRotation(nextDir));
+            // transform.rotation = ;
+        // }
+
+        // transform.Rotate(0, horizontal, 0);
+
+        // Apply gravity
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
